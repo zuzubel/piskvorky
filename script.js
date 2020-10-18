6546e0cab775ff93d71ff88fc1d2234d3e4d92d8
Zuzana Belaskova <z.belaskova@gmail.com>
2020-10-18 21:45:59 +0200

    Hrací pole vypisuje X a O

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let currentPlayer = 'circle';
+const gameArea = document.querySelector('.gameArea');
+let currentPlayerImg = document.querySelector('#currentPlayerImg');
+
+gameArea.addEventListener('click', (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.className = 'board__field--circle';
+    currentPlayer = 'cross';
+    currentPlayerImg.src = 'cross.svg';
+  } else if (currentPlayer === 'cross') {
+    event.target.className = 'board__field--cross';
+    currentPlayer = 'circle';
+    currentPlayerImg.src = 'circle.svg';
+  }
+});
+
+/* dodělat bod VI. aby nešly přepisovat už vybrané tlačítka */
