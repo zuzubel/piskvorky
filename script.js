0282280a9df9b839904f9e99eb3a7ebcaa5cfefd
Zuzana Belaskova <z.belaskova@gmail.com>
2020-10-20 13:26:42 +0200

    Přidán atribut disabled

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 'use strict';
 
+const button = document.querySelector('.gameAreaButton');
+
 let currentPlayer = 'circle';
 const gameArea = document.querySelector('.gameArea');
 let currentPlayerImg = document.querySelector('#currentPlayerImg');
 
 gameArea.addEventListener('click', (event) => {
-  if (
-    event.target.className === 'board__field--circle' ||
-    event.target.className === 'board__field--cross'
-  ) {
-    console.log('sorry plno');
-  } else if (currentPlayer === 'circle') {
+  if (currentPlayer === 'circle') {
+    event.target.setAttribute("disabled", true);
     event.target.className = 'board__field--circle';
     currentPlayer = 'cross';
     currentPlayerImg.src = 'cross.svg';
   } else if (currentPlayer === 'cross') {
+    event.target.setAttribute("disabled", true);
     event.target.className = 'board__field--cross';
     currentPlayer = 'circle';
     currentPlayerImg.src = 'circle.svg';
-  }
+  } 
 });
-
-/* dodělat bod VI. aby nešly přepisovat už vybrané tlačítka */
